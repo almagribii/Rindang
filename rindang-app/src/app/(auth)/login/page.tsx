d7ec8d955bc7733293a9f1b96ad176a07ceee023
Almagribii <brucadalm@gmail.com>
2025-10-05 10:22:22 +0700

    add page and modif page + layout main

---
+++ rindang-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,23 @@
+// app/(auth)/login/page.tsx (Server Component)
+
+import LoginForm from '@/components/auth/LoginForm'
+import { createSupabaseServerClient } from "@/lib/supabase/server-auth"; 
+
+import { redirect } from 'next/navigation'
+
+export default async function LoginPage() {
+  const supabase = createSupabaseServerClient()
+  const { data } = await supabase.auth.getSession()
+
+  if (data.session) {
+    redirect('/dashboard') 
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-[80vh]">
+      <div className="w-full max-w-sm p-8 bg-white rounded-xl shadow-2xl">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
